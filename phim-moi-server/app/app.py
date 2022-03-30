11fd3520e6218e75a0eec9ef2d247c9de7edf73e
Tan Phan <phanminhtan1132000@gmail.com>
2022-03-30 17:56:31 +0700

    Movie feature

--- phim-moi-server/app/app.py
+++ phim-moi-server/app/app.py
@@ -1,66 +1,69 @@
 from flask import Flask, jsonify, request
-import random 
+import random
 
 app = Flask(__name__,
             static_folder='static',
             static_url_path='/static')
 
 IMG_PATH = '/static/img'
+VIDEO_PATH = '/static/video'
 
 @app.route('/')
 def index():
-    pass 
+    pass
 
-@app.route('/api/movie/<name>')
-def show_movie_detail(name):
+
+@app.route('/api/movie/<videoID>')
+def show_movie_detail(videoID):
     return jsonify({
-        'content' : {
-            'name' : name
+        'content': {
+            'src': f'{VIDEO_PATH}/{videoID}.mp4'
         }
     })
+
 
 @app.route('/api/movies')
 def movie():
     return jsonify({
         'content': [
             {
-                'id' : str(random.randrange(10e10)),
-                'title': 'Spider Man: No way home',
-                'author': 'Marvel Studio',
-                'cover': IMG_PATH + '/spiderman3.jpg'
-            },
-            {
-                'id' : str(random.randrange(10e10)),
-                'title': 'Spider Man: Far from home',
-                'author': 'Marvel Studio',
-                'cover': IMG_PATH + '/spiderman2.jpg'
-            },
-            {
-                'id' : str(random.randrange(10e10)),
+                'id': '1',
                 'title': 'Spider Man: Homecoming',
                 'author': 'Marvel Studio',
                 'cover': IMG_PATH + '/spiderman1.jpg'
             },
             {
-                'id' : str(random.randrange(10e10)),
+                'id': '2',
+                'title': 'Spider Man: Far from home',
+                'author': 'Marvel Studio',
+                'cover': IMG_PATH + '/spiderman2.jpg'
+            },
+            {
+                'id': '3',
+                'title': 'Spider Man: No way home',
+                'author': 'Marvel Studio',
+                'cover': IMG_PATH + '/spiderman3.jpg'
+            },
+            {
+                'id': '4',
                 'title': 'Dune',
                 'author': 'Warner Bros. Picture',
                 'cover': IMG_PATH + '/dune.jpg'
             },
             {
-                'id' : str(random.randrange(10e10)),
+                'id': '5',
                 'title': 'Arcane',
                 'author': 'Riot',
                 'cover': IMG_PATH + '/arcane.jpg'
             },
             {
-                'id' : str(random.randrange(10e10)),
+                'id': '6',
                 'title': 'Kimetsu no Yaiba',
                 'author': 'Gotouge Koyoharu',
                 'cover': IMG_PATH + '/kimetsunoyaiba.jpg'
             },
             {
-                'id' : str(random.randrange(10e10)),
+                'id': '7',
                 'title': 'Jujutsu Kaisen',
                 'author': 'Seong-Hu Park',
                 'cover': IMG_PATH + '/jujutsukaisen.jpg'
